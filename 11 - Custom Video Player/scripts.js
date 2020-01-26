5d28f883e0b768845489f4a2ccdabd54e9ce7240
Morgane Gedio <morgane.gedio@gmail.com>
2020-01-26 14:16:01 +0100

    complete the 12 first JS challenge

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,79 @@
+// get elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// build functions
+
+function togglePlay() {
+  // paused is a property that lives on the video - no playing property
+  // const method = video.paused ? 'play' : "pause";
+  // video[method]();
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  // this bounds to video itself
+  const icon = this.paused ? '►' : '❚❚';
+  toggle.textContent = icon;
+}
+
+// to skip some sec en amont ou aval
+function skip() {
+  // object with skip value in it
+  // console.log(this.dataset.skip);
+  // parseFloat : string converted in number
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// to update volume & speed range
+function handleRangeUpdate() {
+  video[this.name] = this.value
+  // console.log(this.name);
+  // console.log(this.value);
+}
+
+// avancer la video en changeant la progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime
+}
+
+// hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+// when someone is moving the mouse, first check the variable mousedown, if true, scrub
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+
